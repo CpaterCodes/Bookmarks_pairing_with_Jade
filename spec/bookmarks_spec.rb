17af59f07059698d16158ab15f45ad7fc74b7544
Charlie Paterson <Charlieed.paterson@googlemail.com>
2019-12-03 16:12:51 +0000

    Linked database to TablePlus, passed spec for new addition

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -5,9 +5,10 @@ describe Bookmarks do
   describe "#all" do
     it "should display all my bookmarks" do
       bookmarks = Bookmarks.all
-      
+
       expect(bookmarks).to include("http://makersacademy.com")
       expect(bookmarks).to include("http://google.com")
+      expect(bookmarks).to include("http://youtube.com")
     end
   end
 
