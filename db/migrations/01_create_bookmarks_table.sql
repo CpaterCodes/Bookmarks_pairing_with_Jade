69b427457427f935f98fd17098dc28271d5417aa
Charlie Paterson <Charlieed.paterson@googlemail.com>
2019-12-03 14:50:16 +0000

    Manipulated database and put instructions into sql file

--- db/migrations/01_create_bookmarks_table.sql
+++ db/migrations/01_create_bookmarks_table.sql
@@ -1,1 +1,3 @@
 CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url  VARCHAR(60));
+INSERT INTO bookmarks(url)
+VALUES('http://destroyallsoftware.com'),('http://google.com'),('http://duckduckgo.com'),('http://makersacademy.com');
